9d381ccb78d40161bc742e65afd3124f83d0df11
Benjamin Quintero <bquinteromdc@gmail.com>
2026-09-15 06:25:07 -0400

    pretty much done

--- scripts/script.js
+++ scripts/script.js
@@ -4,3 +4,12 @@ const navLinks = document.querySelector('.nav-links');
 menuIcon.onclick = () => {
     navLinks.classList.toggle('active');
 }
+
+const projectCards = document.querySelectorAll('.project-card');
+
+projectCards.forEach((card) => {
+    card.addEventListener('click', (e) => {
+        if (e.target.closest('a, .btn')) return;
+        card.classList.toggle('flipped');
+    });
+});
